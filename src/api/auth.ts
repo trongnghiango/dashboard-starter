167f1b5a13ba53eb0918d8cd842a07ae321b7e18
Ka ThinkLap <trongnghia.ngo@hotmail.com>
2025-02-15 16:30:15 +0700

    create file auth.ts to demo request login va getUser

---
+++ src/api/auth.ts
@@ -0,0 +1,30 @@
+import { User } from '../routes/protected-route';
+
+const user: User = {
+  id: 1000,
+  name: 'ABC',
+  role: 'admin'
+};
+export async function getUser(): Promise<any> {
+  //handle
+  // if (!token) return [400, {}];
+  // gia lap thoi gian de thuc hien xong 1 request
+  new Promise((resolve) => setTimeout(resolve, 1000)); // waiting 1000ms
+
+  const authToken = generateAuthToken();
+
+  return [200, { authToken, user }] as const;
+}
+
+function generateAuthToken() {
+  //handle
+  return Math.random().toString(36).substring(2);
+}
+
+export async function login() {
+  new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const authToken = generateAuthToken();
+
+  return [200, { authToken, user }] as const;
+}
